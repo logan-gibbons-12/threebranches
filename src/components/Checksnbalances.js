15b11eff7e64173b43feef16c9104d2afa5b80b7
6-I-X <ljg2024@icloud.com>
2023-02-05 20:23:23 -0500

    finishing up

--- src/components/Checksnbalances.js
+++ src/components/Checksnbalances.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
 
 const Checksnbalances = () => {
-  return (
-    <div>Checksnbalances</div>
-  )
-}
+	
+	return (
+		<div className='chxnbal__comp'>
+			<div className="chxnbaltext">
+				Checks and balances is a system in the US federal government that
+				ensures that no one branch of government becomes too powerful. This
+				system allows each branch to limit the powers of the other two branches,
+				creating a balance of power among them. This system of checks and
+				balances helps to ensure that no one branch of government becomes too
+				powerful, and that each branch serves as a check on the others. It is a
+				fundamental aspect of the US system of government and helps to maintain
+				the separation of powers and promote stability, accountability, and
+				fairness. Examples of checks and balances in the US government include:
+				<ul className="examples">
+					<li>
+						Legislative Branch: The legislative branch has the power to override
+						a Presidential veto with a two-thirds vote, as well as the power to
+						impeach and remove the President or federal judges from office.
+					</li>
+					<li>
+						Executive Branch: The President has the power to veto laws passed by
+						Congress, and to appoint federal judges and officials with the
+						advice and consent of the Senat
+					</li>
+					<li>
+						Judicial Branch: The judicial branch has the power of judicial
+						review, allowing it to interpret the Constitution and declare acts
+						of the legislative or executive branch unconstitutional.
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+};
 
-export default Checksnbalances
+export default Checksnbalances;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,17 +15,4 @@ root.render(
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC62gOfbyyuUYi-lQ_EJgVxUo7gVWoPmhQ",
-  authDomain: "apgovwebsite-21585.firebaseapp.com",
-  projectId: "apgovwebsite-21585",
-  storageBucket: "apgovwebsite-21585.appspot.com",
-  messagingSenderId: "348830971217",
-  appId: "1:348830971217:web:a5e08a1086ba6bac92cf70",
-  measurementId: "G-RDXT5CN10F"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
