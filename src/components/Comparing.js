35b86031a72b60d92b1eb573cd0abb977e86098a
6-I-X <ljg2024@icloud.com>
2023-02-05 22:22:08 -0500

    Finished!!!

--- src/components/Comparing.js
+++ src/components/Comparing.js
@@ -2,8 +2,25 @@ import React from 'react'
 
 const Comparing = () => {
   return (
-    <div className='comparing__comp'>Comparing</div>
-  )
+		<div className="comparing__comp">
+			<div className='comparing_title'>Conclusion</div>
+			<div className="comparingtext">
+				The three branches of government in the United States each have their
+				own distinct responsibilities and operate independently from one
+				another, but their successes are interdependent. The concept of checks
+				and balances and the separation of powers ensures that no single branch
+				becomes too powerful and that each branch has the ability to limit the
+				power of the others. For example, the legislative branch has the power
+				to make laws, but the executive branch has the power to veto those laws,
+				and the judicial branch has the power to interpret laws and ensure they
+				are in accordance with the Constitution. This interplay between the
+				branches creates a balance of power, preventing any one branch from
+				dominating the others. By working together, the three branches ensure
+				that the government operates in an effective and democratic manner, with
+				each branch serving as a check on the power of the others.
+			</div>
+		</div>
+	);
 }
 
 export default Comparing
